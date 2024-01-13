85ebf56bbf101f4676a64afe26920ec4eaaa1685
Nicolas Picotto <86528557+NicoPicotto@users.noreply.github.com>
2024-01-13 15:33:12 +0000

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("hola desde codesandbox baby");
+  const section = document.createElement("div");
+  section.className = "section";
+
+  const title = document.createElement("div");
+  title.className = "title";
+  title.textContent = "Contador de donaciones";
+  section.appendChild(title);
+
+  const ongContainer = document.createElement("div");
+  ongContainer.className = "ong-container";
+  section.appendChild(ongContainer);
+
+  // Aquí puedes agregar los elementos "ong-card" de manera similar
+
+  // Agregar la estructura al DOM
+  const logoContainer = document.getElementById("logo");
+  logoContainer.parentNode.insertBefore(section, logoContainer.nextSibling);
+
   fetch(
     "https://script.google.com/macros/s/AKfycbwnAtMwYh6vgn0x1S8FY3LdrxQdNV6uZkj9hoxR4hqjKGvcPIkQs6ls7VDRkQ3APySoQA/exec",
   )
